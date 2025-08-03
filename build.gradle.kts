d411c441c32194678655f606687530a622b16d39
mailebrilhante <155116907+mailebrilhante@users.noreply.github.com>
2025-08-03 13:02:02 -0600

    Cleaned up code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    application
 }
 
 group = "org.example"
@@ -16,6 +17,11 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(17)
+}
+
+application {
+    mainClass.set("org.example.MainKt")
 }
